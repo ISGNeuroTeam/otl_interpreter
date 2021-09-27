a943304d882068aa24fb96bae00bc690b74a270d
Artem Zenkov <azenkov@isgneuro.com>
2021-09-27 18:10:52 +0700

    refactor command_tree.py

--- otl_interpreter/job_planner/command_tree.py
+++ otl_interpreter/job_planner/command_tree.py
@@ -2,26 +2,29 @@ class CommandTree(object):
 
     def __init__(
             self, command,
-            pipeline_command_tree=None,
-            subsearch_command_trees=None, 
+            previous_command_tree_in_pipeline=None,
+            subsearch_command_trees=None,
             awaited_command_trees=None,
-            next_command_tree=None
+            next_command_tree_in_pipeline=None
     ):
         """
         :param command: Command object
-        :param pipeline_command_tree: Command Tree from  pipeline
+        :param previous_command_tree_in_pipeline: Command Tree from  pipeline
         :param subsearch_command_trees: list of commands awaited for dataframe 
         :param awaited_command_trees: awaited list of command trees
-        :param next_command_tree: next Command object for execution
+        :param next_command_tree_in_pipeline: next Command object for execution
         """
         self.command = command
-        self.pipeline_command_tree = None
+        self.previous_command_tree_in_pipeline = None
         self.subsearch_command_trees = []
         self.awaited_command_trees = []
-        self.next_command_tree = None
+        self.next_command_tree_in_pipeline = None
 
-        if pipeline_command_tree is not None:
-            self.set_pipeline_command_tree(pipeline_command_tree)
+        # if previous command tree in pipeline is None then current command tree is first command tree in pipeline
+        self.first_command_tree_in_pipeline = self
+
+        if previous_command_tree_in_pipeline is not None:
+            self.set_previous_command_tree_in_pipeline(previous_command_tree_in_pipeline)
 
         if subsearch_command_trees is not None:
             for subsearch_command in subsearch_command_trees:
@@ -31,14 +34,15 @@ class CommandTree(object):
             for awaited_command_tree in awaited_command_trees:
                 self.add_awaited_command_tree(awaited_command_tree)
 
-        if next_command_tree is not None:
-            self.set_next_command_tree(next_command_tree)
+        if next_command_tree_in_pipeline is not None:
+            self.set_next_command_tree_in_pipeline(next_command_tree_in_pipeline)
 
     def __repr__(self):
         return self.command
 
-    def set_pipeline_command_tree(self, pipeline_command_tree):
-        self.pipeline_command_tree = pipeline_command_tree
+    def set_previous_command_tree_in_pipeline(self, previous_command_tree_in_pipeline):
+        self.previous_command_tree_in_pipeline = previous_command_tree_in_pipeline
+        self.first_command_tree_in_pipeline = previous_command_tree_in_pipeline.first_command_tree_in_pipeline
 
     def add_subsearch_command_tree(self, subsearch_command_trees):
         self.subsearch_command_trees.append(subsearch_command_trees)
@@ -46,6 +50,6 @@ class CommandTree(object):
     def add_awaited_command_tree(self, awaited_command_tree):
         self.awaited_command_trees.append(awaited_command_tree)
 
-    def set_next_command_tree(self, next_command_tree):
-        self.next_command_tree = next_command_tree
+    def set_next_command_tree_in_pipeline(self, next_command_tree_in_pipeline):
+        self.next_command_tree_in_pipeline = next_command_tree_in_pipeline
 
