2f45901f7cf0a6e95edc27a290a18b86b50f3994
Artem Zenkov <azenkov@isgneuro.com>
2021-10-06 22:14:48 +0700

    command args in command tree come from translator

--- otl_interpreter/job_planner/command.py
+++ otl_interpreter/job_planner/command.py
@@ -1,21 +1,3 @@
-from enum import Enum
-
-
-class ArgType:
-    POSITIONAL = 'POSITIONAL'
-    NAMED = 'NAMED'
-    BY = 'BY'
-    AS = 'AS'
-
-
-class Arg:
-    def __init__(self, arg_type, arg_data_type, arg_value, arg_name=None):
-        self.type = arg_type
-        self.data_type = arg_data_type
-        self.value = arg_value
-        self.name = arg_name
-
-
 class Command:
     def __init__(self, name=None, args=None):
         self.name = name or None
@@ -34,20 +16,7 @@ class Command:
 
     @staticmethod
     def _get_args_from_translated_otl(translated_command):
-        args = []
-        for translated_command_arg in translated_command['commandargs']:
-            if translated_command_arg['type'] == 'arg':
-                arg = Command._get_arg(translated_command_arg)
-            elif translated_command_arg['type'] == 'kwarg':
-                arg = Command._get_kwarg(translated_command_arg)
-            elif translated_command_arg['type'] == 'grouped':
-                arg = Command._get_by_arg(translated_command_arg)
-            elif translated_command_arg['type'] == 'named':
-                arg = Command._get_as_arg(translated_command_arg)
-            else:
-                raise SyntaxError('Found unkrown arg type in translated otl')
-            args.append(arg)
-        return args
+        return translated_command['commandargs']
 
     @staticmethod
     def _get_kwarg(translated_command_arg):
