ad29d199b3915a4a09f63d0e3e730f90e112698f
Artem Zenkov <azenkov@isgneuro.com>
2022-03-29 10:58:02 +0700

    add create_db.sh

---
+++ docs/scripts/create_db.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+export PGUSER=postgres
+export PGPASSWORD=postgres
+export PGPORT=5433
+
+psql -U postgres -h localhost -p 5433 << EOF
+create user otl_interpreter with password 'otl_interpreter';
+create database otl_interpreter;
+grant all privileges on database otl_interpreter to otl_interpreter;
+EOF
