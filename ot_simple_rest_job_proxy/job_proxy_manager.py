d8d9f7e22a9c58ee4a5fbf470f22a57712174dfa
Artem Zenkov <azenkov@isgneuro.com>
2022-04-08 17:53:22 +0700

    makejob and checkjob  job_proxy_manager functions

--- ot_simple_rest_job_proxy/job_proxy_manager.py
+++ ot_simple_rest_job_proxy/job_proxy_manager.py
@@ -1,38 +1,109 @@
 import re
+import datetime
+
+from uuid import UUID
 from redis import Redis
 from pottery import RedisDict
 
 from core.settings import REDIS_CONNECTION_STRING
-from otl_interpreter.otl_job_manager import otl_job_manager
+from otl_interpreter.otl_job_manager import otl_job_manager, QueryError
 
 
 query_match_reg = re.compile(r'\s*v2\s*\|')
+
+new_status_to_old_status = {
+    'NEW': 'new',
+    'TRANSLATED': 'running',
+    'PLANNED': 'running',
+    'RUNNING': 'running',
+    'FINISHED': 'success',
+    'CANCELED': 'canceled',
+    'FAILED': 'failed',
+}
 
 
 class JobProxyManager:
     def __init__(self):
         redis = Redis.from_url(REDIS_CONNECTION_STRING)
-        self.query = RedisDict(redis=redis, key='job_proxy_manager_queries')
+        self.new_platform_queries = RedisDict(redis=redis, key='job_proxy_manager_queries')
+        self.new_platform_queries_job_id = RedisDict(redis=redis, key='job_proxy_manager_queries_id')
 
-
-    def makejob(self, otl_query, tws, twf, cache_ttl):
+    def makejob(self, otl_query, user_guid: UUID, tws: str, twf: str, cache_ttl: str):
         """
         Use otl job manager to make a query
         Returns dictionary like makejob endpoint of ot_simple_rest
         """
-        pass
+        otl_query_dict_key = self._make_key_for_query(otl_query, tws, twf)
+        # convert time to datetime
+        tws = datetime.datetime.fromtimestamp(int(tws))
+        twf = datetime.datetime.fromtimestamp(int(twf))
 
-    def checkjob(self):
+        cache_ttl = int(cache_ttl)
+        try:
+            job_id, storage_type, path = otl_job_manager.makejob(otl_query, user_guid, tws, twf, cache_ttl)
+
+            self.new_platform_queries_job_id[job_id.hex] = otl_query_dict_key
+
+            if otl_query_dict_key not in self.new_platform_queries:
+                self.new_platform_queries[otl_query_dict_key] = {
+                    'job_id': job_id.hex,
+                    'storage_type': storage_type,
+                    'path': path,
+                    'status': 'new',
+                    'status_text': 'Job created'
+                }
+        except QueryError as err:
+            self.new_platform_queries[otl_query_dict_key] = {
+                'status': 'failed',
+                'status_text': str(err)
+            }
+            return {'status': 'fail', 'error': str(err)}
+
+        return {'status': 'success', 'timestamp': datetime.datetime.now().isoformat()}
+
+    def checkjob(self, otl_query, tws, twf):
         """
         Use otl job manager to check job status
         Return dectionary like checkjob endpoint of ot_simple_rest
         """
-        pass
+        otl_query_dict_key = self._make_key_for_query(otl_query, tws, twf)
 
-    def getresult(self):
+        if otl_query_dict_key not in self.new_platform_queries:
+            return {"status": "notfound", "error": "Job is not found"}
+        query = self.new_platform_queries[otl_query_dict_key]
+
+        if query['status'] == 'failed':
+            return {'status': 'failed', 'error': query['status_text']}
+
+        job_id = UUID(query['job_id'])
+
+        status, status_text = otl_job_manager.check_job(job_id)
+
+        old_status = new_status_to_old_status[status]
+
+        res = {
+            'status': old_status
+        }
+
+        if old_status in ("failed", "canceled"):
+            res['error'] = status_text
+
+        if old_status == 'success':
+            res['cid'] = query['job_id']
+
+        return res
+
+    def getresult(self, cid):
         """
-        Use otl job manager to get
+        Use otl job manager to get results
         """
+        # if cid not in self.new_platform_queries_job_id:
+        #     return {'status': 'failed', 'error': 'No cache with id=cid '}
+        #
+        #
+        # {"status": "success",
+        #  "data_urls": ["cache/search_59.cache/data/part-00000-34570ec2-3504-4f4e-9ebb-aa58965b8fc9-c000.json",
+        #                "cache/search_59.cache/data/_SCHEMA"]}
         pass
 
     def is_new_platform_query(selfj, query):
@@ -47,7 +118,7 @@ class JobProxyManager:
         """
         Returns true id query with id was sent to new platform
         """
-        return query_id in self.query
+        return query_id in self.new_platform_queries_job_id
 
     def _make_key_for_query(self, query, tws, twf):
         """
