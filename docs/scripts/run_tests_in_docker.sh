14f9d56ad9463b1d7622fbff6005591a22fc2c4b
Artem Zenkov <azenkov@isgneuro.com>
2022-06-27 18:20:22 +0700

    change otlang version to 1.2.0

--- docs/scripts/run_tests_in_docker.sh
+++ docs/scripts/run_tests_in_docker.sh
@@ -2,5 +2,5 @@
 
 cd `dirname "${BASH_SOURCE[0]}"`
 export CURRENT_UID=$(id -u):$(id -g)
-docker-compose -f ../../../../docker-compose-test.yml  run --rm   complex_rest python /complex_rest/complex_rest/manage.py test /complex_rest/plugin_dev/otl_interpreter/tests   --settings=core.settings.test
+docker-compose -f ../../../../docker-compose-test.yml  run --rm   complex_rest python /complex_rest/complex_rest/manage.py test /complex_rest/plugin_dev/otl_interpreter/tests   --settings=core.settings.test --no-input
 docker-compose -f ../../../../docker-compose-test.yml down
