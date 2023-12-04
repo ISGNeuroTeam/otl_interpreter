b0c9c1b18079af42dde6ad69d1abb64943ea6990
Artem Zenkov <azenkov@isgneuro.com>
2023-12-04 17:20:32 +0700

    wait for postgre in dispatcher container

--- docs/docker/complex_rest/dispatcher_entrypoint.sh
+++ docs/docker/complex_rest/dispatcher_entrypoint.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
 
-echo "Waiting for postgres and kafka..."
-sleep 90
-
+export PGPASSWORD=otl_interpreter
+until psql  -c "select 1;" -U otl_interpreter -h postgres -c "select 1;" > /dev/null 2>&1; do
+  echo "Waiting for postgres server"
+  sleep 5
+done
+echo "Starting"
 
 exec "$@"
